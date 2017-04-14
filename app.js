3eaa5be6f3d717210bab4b965da8270d535f4e97
byoungjunjo <byoungjunjo@gmail.com>
2017-04-13 18:21:02 -0700

    Adding Update and Delete Route

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var expressSanitizer = require("express-sanitizer"),
     bodyParser       = require("body-parser"),
     mongoose         = require("mongoose"),
     express          = require("express"),
+    Comment          = require("./models/comment");
     Story            = require("./models/story");
+
 app = express();
 
 // App Configuration
@@ -13,20 +15,6 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
 app.use(methodOverride("_method"));
-
-// DB Testing
-// Story.create(
-//   {
-//     title: "Post one",
-//     image: "https://farm4.staticflickr.com/3373/3600836516_ab924c6729.jpg",
-//     description: "Lorem impsum memento mori una ragazza mio roemo mi casa su casa."
-//   }, function(err, newlyCreatedStory){
-//      if(err){
-//       console.log(err);
-//      } else {
-//        console.log("Newly Created Story: \t" + newlyCreatedStory);
-//      }
-//   });
 
 // RESTFUL routes
 // landing page
@@ -52,6 +40,8 @@ app.get("/stories/new", function(req, res){
 
 // CREATE
 app.post("/stories", function(req, res){
+  req.body.story.body = req.sanitize(req.body.story.body); // use a middleware later
+  console.log(req.body.story.body);
   Story.create(req.body.story, function(err, newStory){
     if(err){
       res.render("new");
@@ -63,11 +53,45 @@ app.post("/stories", function(req, res){
 
 // SHOW
 app.get("/stories/:id", function(req,res){
-  Story.findById(req.params.id, function(err, foundStory){
+  Story.findById(req.params.id).populate("comments").exec(function(err, foundStory){
     if(err){
       res.redirect("/stories");
     } else {
       res.render("show", {story: foundStory});
+    }
+  });
+});
+
+// EDIT
+app.get("/stories/:id/edit", function(req, res){
+  Story.findById(req.params.id, function(err, foundStory){
+    if(err){
+      res.redirect("/stories");
+    } else {
+      res.render("edit", {story: foundStory});
+    }
+  });
+});
+
+// UPDATE
+app.put("/stories/:id", function(req, res){
+  req.body.story.body = req.sanitize(req.body.story.body); // use a middleware later
+  Story.findByIdAndUpdate(req.params.id, req.body.story, function(err, updatedStory){
+    if(err){
+      res.redirect("back");
+    } else {
+      res.redirect("/stories/" + req.params.id);
+    }
+  });
+});
+
+// DELETE
+app.delete("/stories/:id", function(req, res){
+  Story.findByIdAndRemove(req.params.id, function(err, removedStory){
+    if(err){
+      res.redirect("back");
+    } else {
+      res.redirect("/stories");
     }
   });
 });
